3525f121a411318697125c1d46709fe22bf4b9a2
Jake Robson <jrobsonsws@gmail.com>
2019-09-03 20:20:36 +0100

    Added screenshot functionality. Added new field "Project Name" which is used for the screenshot filenames. Folder select entry is now disabled and filepaths can only be added using the browse button. Added imports for desktopmagic and os. Added requirements.txt

--- autoscreengrab.py
+++ autoscreengrab.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
+from desktopmagic.screengrab_win32 import getDisplayRects, getRectAsImage
+import os
 
 class mainWindow:
     #? Variables
@@ -7,6 +9,7 @@ class mainWindow:
     WIDTH = 400
     running = True
     callback = None
+    screenshotCount = 1
 
     def __init__(self):
         #! PRESENTATION LAYER !#
@@ -22,7 +25,7 @@ class mainWindow:
         self.frameFolderSelect.place(relx=0.5, rely=0.1, relwidth=0.9, relheight=0.2, anchor='n')
         self.lblFolderSelect = tk.Label(self.frameFolderSelect, text='Save Location')
         self.lblFolderSelect.place(relx=0, rely=0, relwidth=0.25, relheight=0.25, anchor='nw')
-        self.entryFolderSelect = tk.Entry(self.frameFolderSelect)
+        self.entryFolderSelect = tk.Entry(self.frameFolderSelect, state="readonly")
         self.entryFolderSelect.place(relx=0, rely=0.5, relwidth=0.7, relheight=0.5, anchor='w')
         self.btnFolderSelect = tk.Button(self.frameFolderSelect, text='browse', command = self.BtnFolderSelectPressed)
         self.btnFolderSelect.place(relx=0.75, rely=0.5, relwidth=0.25, relheight=0.5, anchor='w')
@@ -30,13 +33,17 @@ class mainWindow:
         #? Interval
         self.frameInterval = tk.Frame(self.root)
         self.frameInterval.place(relx=0.5, rely=0.3, relwidth=0.9, relheight=0.2, anchor='n')
-        self.lblInterval = tk.Label(self.frameInterval, text='Interval')
-        self.lblInterval.place(relx=0, rely=0, relwidth=0.15, relheight=0.25, anchor='nw')
+        self.lblInterval = tk.Label(self.frameInterval, text='Interval (mins)')
+        self.lblInterval.place(relx=0, rely=0, relwidth=0.25, relheight=0.25, anchor='nw')
         self.entryInterval = tk.Entry(self.frameInterval, justify='center')
-        self.entryInterval.place(relx=0, rely=0.5, relwidth=0.2, relheight=0.5, anchor='w')
+        self.entryInterval.place(relx=0, rely=0.5, relwidth=0.25, relheight=0.5, anchor='w')
         self.entryInterval.insert(0, '1')
-        self.lblMinutes = tk.Label(self.frameInterval, text='minutes')
-        self.lblMinutes.place(relx=0.25, rely=0.5, relwidth=0.15, relheight=0.5, anchor='w')
+
+        #? Project name
+        self.lblProjectName = tk.Label(self.frameInterval, text='Project Name')
+        self.lblProjectName.place(relx=0.3, rely=0, relwidth=0.25, relheight=0.25, anchor='nw')
+        self.entryProjectName = tk.Entry(self.frameInterval)
+        self.entryProjectName.place(relx=0.3, rely=0.5, relwidth=0.7, relheight=0.5, anchor='w')
 
         #? Info section
         self.frameInfo = tk.Frame(self.root)
@@ -54,53 +61,66 @@ class mainWindow:
         self.btnStop = tk.Button(self.frameStartStop, text='stop', state='disabled', command = self.BtnStopPressed)
         self.btnStop.place(relx=0.6, rely=0.5, relwidth=0.4, relheight=0.5, anchor='w')
 
-
         #? End root
         self.root.mainloop()
 
     #! BUSINESS LAYER !#
     def BtnFolderSelectPressed(self):
-        file_path = filedialog.askdirectory()
+        self.entryFolderSelect.config(state='normal')
+        filePath = filedialog.askdirectory()
         self.entryFolderSelect.delete(0, 'end')
-        self.entryFolderSelect.insert(0, file_path)
+        self.entryFolderSelect.insert(0, filePath)
+        self.entryFolderSelect.config(state='readonly')
 
     def BtnStartPressed(self):
-        #TODO: Take a screenshot immediately
+        interval = int(self.entryInterval.get())
+        self.running = True
         self.lblScreenshotsTaken.config(text='Screenshots taken: 0')
         #? Enable stop button
         self.btnStop.config(state='active')
-        #? Disable start button, browse button, interval entry
+        #? Disable start button, browse button, interval entry, project name
         self.btnStart.config(state='disabled')
         self.btnFolderSelect.config(state='disabled')
         self.entryInterval.config(state='readonly')
+        self.entryProjectName.config(state='readonly')
         #TODO: Start timer
+        #? Take screenshot
+        self.TakeScreenshot(interval)
         #? Start updating screenshots taken
-        self.running = True
-        self.callback = self.UpdateLblScreenshotsTaken(interval=int(self.entryInterval.get()))
+        self.UpdateLblScreenshotsTaken(interval)
 
     def BtnStopPressed(self):
         #? Disable stop button
         self.btnStop.config(state='disabled')
-        #? Enable start button, browse button, interval entry
+        #? Enable start button, browse button, interval entry, project name
         self.btnStart.config(state='active')
         self.btnFolderSelect.config(state='active')
         self.entryInterval.config(state='normal')
+        self.entryProjectName.config(state='normal')
         #TODO: open popup window prompting to open screenshot folder
         #? Stop updating screenshots taken
         self.running = False
         self.root.after_cancel(self.callback)
+        #? Reset screenshot count back to 1
+        self.screenshotCount = 1
 
     def UpdateLblScreenshotsTaken(self, interval):
         if self.running:
             screenshots_taken = int(self.lblScreenshotsTaken.cget('text').split(':')[1].strip())
             screenshots_taken = screenshots_taken + 1
-            self.lblScreenshotsTaken.config(text=f'Screenshots taken: {str(screenshots_taken)}')
             #! change interval multiplication back to *60000
-            self.callback = self.root.after(interval*600, lambda: self.UpdateLblScreenshotsTaken(int(self.entryInterval.get())))
+            self.lblScreenshotsTaken.config(text=f'Screenshots taken: {str(screenshots_taken)}')
+            self.callback = self.root.after(interval*6000, lambda: self.UpdateLblScreenshotsTaken(int(self.entryInterval.get())))
 
-    def TakeScreenshot(self):
-        #TODO: The whole bloody lot
-        pass
+    def TakeScreenshot(self, interval):
+        if self.running:
+            screen = getRectAsImage(getDisplayRects()[1])
+            filePath = self.entryFolderSelect.get()
+            fileName = self.entryProjectName.get() + '_' + str(self.screenshotCount) + '.png'
+            screen.save(os.path.join(filePath, fileName), format='png')
+            self.screenshotCount += 1
+            #! change interval multiplication back to *60000
+            self.callback = self.root.after(interval*6000, lambda: self.TakeScreenshot(int(self.entryInterval.get())))
 
 if __name__ == '__main__':
     mainWindow = mainWindow()
