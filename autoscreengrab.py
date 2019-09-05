296a15456dd496751ecbc708e8e618485ac70473
Jake Robson <jrobsonsws@gmail.com>
2019-09-05 20:02:48 +0100

    Added feature that checks folder for files that match the project name and carries on from where it left off. This introduced a bug where the screenshots label taken is 1 higher than the actual screenshots taken.  I know why it is doing it but I can't think of a clean solution yet.

--- autoscreengrab.py
+++ autoscreengrab.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from desktopmagic.screengrab_win32 import getDisplayRects, getRectAsImage
 import os
+import glob
 
 
 class MainWindow:
@@ -10,7 +11,7 @@ class MainWindow:
     WIDTH = 400
     running = True
     callbacks = []
-    screenshotCount = 1
+    screenshot_count = 0
 
     def __init__(self):
         # ! PRESENTATION LAYER !#
@@ -86,6 +87,14 @@ class MainWindow:
         self.entry_interval.config(state='readonly')
         self.entry_project_name.config(state='readonly')
         # TODO: Start timer
+        # ? Check folder for project files with the same name and set screenshots_taken variable
+        # ! If this returns true it sets the screenshots taken to 1 more than there is - need to fix
+        file_path = self.entry_folder_select.get()
+        project_name = self.entry_project_name.get()
+        tmp_screenshots_taken = self.check_folder_for_files(file_path, project_name)
+        if tmp_screenshots_taken is not None:
+            self.screenshot_count = tmp_screenshots_taken
+            self.lbl_screenshots_taken.config(text=f'Screenshots taken: {str(self.screenshot_count)}')
         # ? Take screenshot
         self.take_screenshot(interval)
         # ? Start updating screenshots taken
@@ -105,7 +114,7 @@ class MainWindow:
         for callback in self.callbacks:
             self.root.after_cancel(callback)
         # ? Reset screenshot count back to 1
-        self.screenshotCount = 1
+        self.screenshot_count = 1
 
     def update_lbl_screenshots_taken(self, interval):
         if self.running:
@@ -113,20 +122,34 @@ class MainWindow:
             screenshots_taken = screenshots_taken + 1
             # ! change interval multiplication back to *60000
             self.lbl_screenshots_taken.config(text=f'Screenshots taken: {str(screenshots_taken)}')
-            self.callbacks.append(self.root.after(interval * 60000,
+            self.callbacks.append(self.root.after(interval * 6000,
                                                   lambda: self.update_lbl_screenshots_taken(
                                                       int(self.entry_interval.get()))))
 
     def take_screenshot(self, interval):
+        if self.screenshot_count == 0:
+            self.screenshot_count = 1
         if self.running:
             screen = getRectAsImage(getDisplayRects()[0])
             file_path = self.entry_folder_select.get()
-            file_name = self.entry_project_name.get() + '_' + str(self.screenshotCount) + '.png'
+            file_name = self.entry_project_name.get() + '_' + str(self.screenshot_count) + '.png'
             screen.save(os.path.join(file_path, file_name), format='png')
-            self.screenshotCount += 1
+            self.screenshot_count += 1
             # ! change interval multiplication back to *60000
-            self.callbacks.append(self.root.after(interval * 60000,
+            self.callbacks.append(self.root.after(interval * 6000,
                                                   lambda: self.take_screenshot(int(self.entry_interval.get()))))
+
+    @staticmethod
+    def check_folder_for_files(folder_path, project_name):
+        list_dir = os.listdir(folder_path)
+        list_dir = [file.lower() for file in glob.glob(f'{os.path.join(folder_path, project_name)}*.png')]
+        if len(list_dir) != 0:
+            list_dir.sort(key=lambda x: os.path.getctime(x))
+            last_file = list_dir[-1]
+            last_file_number = last_file.split('_')[-1]
+            last_file_number = int(last_file_number.split('.')[0]) + 1
+            return last_file_number
+        return None
 
 
 if __name__ == '__main__':
